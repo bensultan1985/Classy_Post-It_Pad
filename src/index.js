d449c6c4ae0297f20e837130c296a05761ffb119
bensultan1985 <bensultan1985@gmail.com>
2020-06-08 16:10:54 -0700

    added express

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import express from 'express';
+import path from 'path';
+
+var app = express();
+
+
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000);
+
+console.log('server started on port: ', process.env.PORT || 3000);
 
 ReactDOM.render(
     <App />,
