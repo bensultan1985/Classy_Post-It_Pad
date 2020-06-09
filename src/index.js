cf1bec7066e5bfbf18018640bd2f091f8f1f9bca
bensultan1985 <bensultan1985@gmail.com>
2020-06-08 18:25:01 -0700

    more changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import express from 'express';
+import path from 'path';
+import  favicon from 'serve-favicon'
+
+var app = express();
+
+const router = express.Router()
+
+// app.use(favicon(path.join(__dirname,'favicon.ico')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(process.env.PORT || 3000);
+
+console.log('server started on port: ', process.env.PORT || 3000);
+
 
 ReactDOM.render(
     <App />,
