6deb27a46d6a470c2385948eaca5a8f4c4106b49
bensultan1985 <bensultan1985@gmail.com>
2020-06-16 15:51:32 -0700

    added local storage

--- src/App.js
+++ src/App.js
@@ -20,14 +20,50 @@ class App extends React.Component {
   this.delNote = this.delNote.bind(this);
 }
 
-  componentWillMount() {
+  async componentWillMount () {
+    console.log('running')
     console.log(this.state)
+    if (await localStorage) {
+      let allLocalStorage = Object.keys(localStorage)
+      allLocalStorage.forEach(element => {
+        this.setState({
+          notes: {
+            ...this.state.notes,
+            [element]: localStorage.getItem(element)
+        }
+        })
+      })
+        // console.log(localStorage.getItem(allLocalStorage[element]))
+
+    console.log(this.state.notes)
+    }
   }
+
+
+  //why does this not work??
+
+  // componentWillMount() {
+  //   console.log(this.state)
+  //   if (localStorage) {
+  //     let allLocalStorage = Object.keys(localStorage)
+  //     allLocalStorage.forEach(element => {
+  //       // console.log(localStorage.getItem(allLocalStorage[element]))
+  //       this.setState({
+  //         notes: {
+  //           ...this.state.notes,
+  //         [element]: localStorage.getItem(element)
+  //       }
+  //       })
+  //     })
+  //   console.log(this.state.notes)
+  //   }
+  // }
 
   makeNew = () => {
     let newCount = this.state.count + 1
     this.setState({count: newCount})
     let test = uuid()
+    localStorage.setItem([test], '')
     this.setState({notes: {...this.state.notes,
       [test]: ''}})
 
@@ -46,15 +82,17 @@ class App extends React.Component {
 
       //deletes the note
     this.setState({...delete this.state.notes[noteKey]})
+    localStorage.removeItem(noteKey);
   }
 
 
     render() {
+      console.log(this.state)
       return (
       <div className="App">
         <div id="header">Classy Post-It Pad</div>
         <Button makeNew={this.makeNew} noteObj={this.state}/>
-        {Object.keys(this.state.notes).map(element => <Note key={element} note={element} delNote={this.delNote}/>)}
+        {Object.keys(this.state.notes).map(element => <Note key={element} note={element} text={this.state.notes[element]} message={element} delNote={this.delNote}/>)}
       </div>
     );
   }
--- src/note.js
+++ src/note.js
@@ -7,10 +7,16 @@ class Note extends React.Component {
     super(props)
   }
 
+  updateSession = (e) => {
+    e.preventDefault()
+    console.log('updating note')
+    localStorage.setItem([this.props.note], document.getElementById(this.props.note).value);
+  }
+
     render() {
       return (
       <div className="note">
-        <textarea placeholder="type your note..."></textarea>
+        <textarea id={this.props.note} placeholder="type your note..." defaultValue={this.props.text} message={this.props.message} onChange={this.updateSession}></textarea>
         <DelButton note={this.props.note} delNote={this.props.delNote}/>
       </div>
     );
