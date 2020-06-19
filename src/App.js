f7b1f1248709ab6b2d076b2f6817d10c47aead2a
bensultan1985 <bensultan1985@gmail.com>
2020-06-19 14:36:37 -0700

    changed text intro

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App extends React.Component {
 
   async componentWillMount () {
     console.log('running')
+    if (await localStorage.getItem('gotHelp') == 'true') {
+      console.log(localStorage.gotHelp)
+      document.getElementById('helpheader').style.display = 'none'
+    } else {
+      document.getElementById('helpheader').style.display = 'block';
+    }
     console.log(this.state)
     if (await localStorage) {
       let allLocalStorage = Object.keys(localStorage)
@@ -62,7 +68,7 @@ class App extends React.Component {
   makeNew = () => {
     let newCount = this.state.count + 1
     this.setState({count: newCount})
-    let test = uuid()
+    let test = new Date().getTime()
     localStorage.setItem([test], '')
     this.setState({notes: {...this.state.notes,
       [test]: ''}})
@@ -85,14 +91,26 @@ class App extends React.Component {
     localStorage.removeItem(noteKey);
   }
 
+  noHelp = () => {
+    localStorage.setItem('gotHelp', 'true')
+    document.getElementById('helpheader').style.display = 'none';
+  }
+
 
     render() {
       console.log(this.state)
       return (
       <div className="App">
+        <div id="helpheader">
+          Bonjour, Ciao, Namaste, Ni Hau. This classy notepad is for the most elegant, articulate mavens upon polymaths. You have worldly problems to solve and grocery lists to write. Waste not your brain power on wondering if you've saved your notes. Every letter you enter is auto-saved, without fail. Write a note. Close your browser. Open it back up next week - hell - next year! Voila! Your cherished note will be here to greet you like an old friend.
+          <button id="closehelpheader" onClick={this.noHelp}> got it </button>
+        </div>
         <div id="header">Classy Post-It Pad</div>
         <Button makeNew={this.makeNew} noteObj={this.state}/>
-        {Object.keys(this.state.notes).map(element => <Note key={element} note={element} text={this.state.notes[element]} message={element} delNote={this.delNote}/>)}
+        {Object.keys(this.state.notes).filter(element => element !== 'gotHelp').map(element => <Note key={element} note={element} text={this.state.notes[element]} message={element} delNote={this.delNote}/>).sort((a,b) => {
+          return b.key - a.key
+        })
+      }
       </div>
     );
   }
